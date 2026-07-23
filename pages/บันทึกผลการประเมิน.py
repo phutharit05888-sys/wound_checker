0a7f75aa15ff96b8de5033e6f52548543edc7601
phutharit05888-sys <phutharit05888@varee.ac.th>
2026-07-23 20:47:30 +0700

    Update บันทึกผลการประเมิน.py

--- pages/บันทึกผลการประเมิน.py
+++ pages/บันทึกผลการประเมิน.py
@@ -2,19 +2,113 @@ import streamlit as st
 import pandas as pd
 import os
 
-st.title("📋 ระบบบันทึกผลการประเมิน")
+st.set_page_config(
+    page_title="บันทึกผลการประเมิน",
+    page_icon="📋",
+    layout="centered"
+)
 
 HISTORY_FILE = "assessment_history.csv"
 
-if os.path.exists(HISTORY_FILE):
+st.markdown("""
+<style>
 
-    df = pd.read_csv(HISTORY_FILE)
+.card{
+    display:flex;
+    background:white;
+    border-radius:20px;
+    overflow:hidden;
+    margin-bottom:18px;
+    box-shadow:0px 2px 10px rgba(0,0,0,0.08);
+}
 
-    st.dataframe(
-        df.drop(columns=["ภาพแผล"]),
-        use_container_width=True
-    )
+.bar{
+    width:18px;
+}
+
+.green{
+    background:#49C16D;
+}
+
+.yellow{
+    background:#FFD23F;
+}
+
+.orange{
+    background:#FF9800;
+}
+
+.red{
+    background:#E53935;
+}
+
+.content{
+    padding:18px;
+}
+
+.title{
+    font-size:28px;
+    font-weight:bold;
+    color:#123B78;
+}
+
+.date{
+    color:#777;
+    font-size:16px;
+    margin-top:5px;
+}
+
+</style>
+""", unsafe_allow_html=True)
+
+st.title("📋 บันทึกผลการประเมิน")
+
+if not os.path.exists(HISTORY_FILE):
+
+    st.info("ยังไม่มีข้อมูลการประเมิน")
 
 else:
 
-    st.info("ยังไม่มีข้อมูลการประเมิน")
+    df = pd.read_csv(HISTORY_FILE)
+
+    df = df.iloc[::-1]
+
+    for _, row in df.iterrows():
+
+        risk = row["ระดับความเสี่ยง"]
+
+        if risk == "Grade 1":
+            color = "green"
+            text = "ควรดูแลเฝ้าระวัง"
+
+        elif risk == "Grade 2":
+            color = "yellow"
+            text = "ควรพบแพทย์"
+
+        elif risk == "Grade 3":
+            color = "orange"
+            text = "ควรพบแพทย์โดยด่วน"
+
+        else:
+            color = "red"
+            text = "ควรพบแพทย์โดยด่วน"
+
+        st.markdown(f"""
+        <div class="card">
+
+            <div class="bar {color}"></div>
+
+            <div class="content">
+
+                <div class="title">
+                    {text}
+                </div>
+
+                <div class="date">
+                    บันทึกวันที่ : {row['วันที่และเวลาที่ประเมิน']}
+                </div>
+
+            </div>
+
+        </div>
+        """, unsafe_allow_html=True)
